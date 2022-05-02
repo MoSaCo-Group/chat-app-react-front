4a1dbb6be92f1f8c9ed4429cd1f179dd2ee8fac1
SakoPak <sako.taya@gmail.com>
2022-05-02 12:21:25 -0700

    Adding Profile component
    Create, Index, Show/Delete, Update,

---
+++ src/components/Profile/IndexProfiles.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import indexProfiles from './components/api/profile'
+
+class IndexProfile extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      profiles: null
+    }
+  }
+
+  componentDidMount () {
+    const { user, msgAlert } = this.props
+
+    indexProfiles(user)
+      .then(res => this.setState({ profiles: res.data.profiles }))
+      .then(() => {
+        msgAlert({
+          heading: 'Index success',
+          message: 'Successfully indexed',
+          variant: 'success'
+        })
+      })
+      .catch(error => {
+        msgAlert({
+          heading: 'Index fail',
+          message: 'Index error: ' + error.message,
+          variant: 'danger'
+        })
+      })
+  }
+
+  render () {
+    const { profiles } = this.state
+
+    if (profiles === null) {
+      return 'Loading...'
+    }
+
+    let profilesJSX
+    if (profiles.length === 0) {
+      profilesJSX = 'No profiles created.'
+    } else {
+      profilesJSX = profiles.map(profile => (
+        <li key={profile._id}>
+          <Link to={`/profiles/${profile._id}`}>{profile.username}
+          </Link>
+        </li>
+      ))
+    }
+    return (
+      <><h3>All Profiles:</h3>
+        <ul>
+          {profilesJSX}
+        </ul>
+
+      </>
+    )
+  }
+}
+
+export default IndexProfile
---
+++ src/components/Profile/ShowProfile.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button'
+import { withRouter } from 'react-router-dom'
+import { showProfile, deleteProfile } from './components/api/profile'
+
+class ShowProfile extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      profile: null
+    }
+  }
+
+  componentDidMount () {
+    const { match, user, msgAlert } = this.props
+
+    showProfile(match.params.id, user)
+      .then((res) => this.setState({ profile: res.data.profile }))
+      .then(() => {
+        msgAlert({
+          heading: 'Show Profile success',
+          message: 'Show Profile success',
+          variant: 'success'
+        })
+      })
+      .catch((error) => {
+        msgAlert({
+          heading: 'Show Profile failed',
+          message: 'Error message: ' + error.message,
+          variant: 'danger'
+        })
+      })
+  }
+
+handleDelete = () => {
+  const { match, user, msgAlert, history } = this.props
+
+  deleteProfile(match.params.id, user)
+    .then(() => history.push('/profiles'))
+    .then(() => {
+      msgAlert({
+        heading: 'Delete success',
+        message: 'Successfully deleted',
+        variant: 'success'
+      })
+    })
+    .catch((error) => {
+      msgAlert({
+        heading: 'Delete fail',
+        message: 'Delete error: ' + error.message,
+        variant: 'danger'
+      })
+    })
+}
+
+render () {
+  if (this.state.profile === null) {
+    return 'Loading...'
+  }
+
+  const { username, first, last, owner } = this.state.profile
+  const { user, history, match } = this.props
+  return (
+    <>
+      <h3>Show Profile</h3>
+      <h4>{username}</h4>
+      <p>First Name: {first}</p>
+      <p>Last Name: {last}</p>
+      {user._id === owner && (
+        <>
+          <Button onClick={this.handleDelete}>Delete</Button>
+          <Button
+            onClick={() => history.push(`/profiles/${match.params.id}/edit`)}>
+                        Update
+          </Button>
+        </>
+      )}
+    </>
+  )
+}
+}
+
+export default withRouter(ShowProfile)
