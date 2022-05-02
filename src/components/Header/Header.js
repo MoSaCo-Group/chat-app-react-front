38f120cc379bfc941bf4cf9f38a6b0b9fb856bfb
ms00l <moesuleiman8@gmail.com>
2022-05-02 13:59:40 -0400

    pain

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link, NavLink } from 'react-router-dom'
-import Logo from './../logo.png'
+import logo from './../logo.png'
 import './Header.css'
+
+const chatOptions = (
+	<Fragment>
+		<NavLink exact to='/chat' className='nav-link'>
+			Chat
+		</NavLink>
+		<NavLink exact to='/profile' className='nav-link'>
+			Profile
+		</NavLink>
+	</Fragment>
+)
 
 const authenticatedOptions = (
 	<Fragment>
@@ -16,12 +28,14 @@ const authenticatedOptions = (
 		<NavLink to='/sign-out' className='nav-link'>
 			Sign Out
 		</NavLink>
+		{chatOptions}
 	</Fragment>
 )
 
 const unauthenticatedOptions = (
 	<Fragment>
-		<NavLink to='/sign-up' className='nav-link'>
+		<NavLink to='/sign-up'
+			className='nav-link'>
 			Sign Up
 		</NavLink>
 		<NavLink to='/sign-in' className='nav-link'>
@@ -38,24 +52,37 @@ const alwaysOptions = (
 	</Fragment>
 )
 
-const Header = ({ user }) => (
-	<Navbar bg='black' expand='xxl'>
-		<Navbar.Brand>
-			<Link to='/'>
-				<img src={Logo} className='logo' />
+function Header ({ user }) {
+	const [isOpen, setIsOpen] = useState(false)
+
+return (
+	<Navbar bg='new' expand='lg'>
+		<Navbar.Brand href='/'>
+			<Link to='/' style={{ textDecoration: 'none', color: '#FFF' }}>
+				<img src={logo} height='150px' width='150px' />
 			</Link>
 		</Navbar.Brand>
-		<Navbar.Toggle aria-controls='basic-navbar-nav' />
-		<Navbar.Collapse id='basic-navbar-nav'>
-			<Nav className='ml-auto'>
-				{user && (
-					<span className='navbar-text mr-2'>Welcome, {user.email}</span>
-				)}
-				{alwaysOptions}
-				{user ? authenticatedOptions : unauthenticatedOptions}
+		<Navbar.Toggle
+			onClick={() => {
+				setIsOpen()
+			}}
+			className='toggle'
+			aria-controls='basic-navbar-nav'>
+			Options
+		</Navbar.Toggle>
+		<Navbar.Collapse isOpen={isOpen} id='basic-navbar-nav'>
+			<Nav>
+				Auth Options
+				<NavDropdown className='ml-auto'>
+					{user && (
+						<span className='navbar-text mr-2'>Welcome, {user.email}</span>
+					)}
+					{alwaysOptions}
+					{user ? authenticatedOptions : unauthenticatedOptions}
+				</NavDropdown>
 			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
 )
-
+					}
 export default Header
