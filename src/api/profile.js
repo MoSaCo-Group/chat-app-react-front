9816066de74ffb8164e8c4dd35ccc2fa2b215704
jackson916 <colingrey412@gmail.com>
2022-05-05 12:23:40 -0400

    commit

--- src/api/profile.js
+++ src/api/profile.js
@@ -54,3 +54,12 @@ export const deleteProfile = (id, user) => {
     }
   })
 }
+export const indexProfiles = (id, user) => {
+  return axios({
+    url: apiUrl + '/profile/',
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
