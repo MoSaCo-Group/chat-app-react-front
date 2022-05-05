42b87b73ee8d11a8f72714800c1fddfefbdf5db3
SakoPak <sako.taya@gmail.com>
2022-05-05 14:03:29 -0700

    edits to Container

--- src/components/Landing/Container.js
+++ src/components/Landing/Container.js
@@ -1,28 +1,34 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import './container.css'
+import { withRouter } from 'react-router-dom'
 
-const Landing = () => {
-  return (
-    <>
-      <Container className='container-fluid'>
-        <Row px-0>
-          <Col className='leftCol' s={6} md={4} px={0}>
-            <p className='landingWord'>MESSAGING MADE EASY</p>
-            <a href='something' className='button1'>
-                    Get Started
-            </a>
-            <a href='something' className='button1'>
-                    Sign In
-            </a>
-          </Col>
-          <Col className='rightCol' s={6} md={4} px={0}>
-                BLANK SPACE
-          </Col>
-        </Row>
-      </Container>
-    </>
-  )
+class Landing extends Component {
+  render () {
+    const { history } = this.props
+
+    return (
+      <>
+        <Container className='container-fluid'>
+          <Row id='homePage' px-0>
+            <Col className='leftCol' s={6} md={4} px={0}>
+              <p className='landingWord'>MESSAGING MADE EASY</p>
+              {/* <a href='/sign-up' onClick="hideMain()" className='button1'>
+                      Get Started
+            </a> */}
+              <Button onClick={() => history.push('/sign-up')} className='button1' type='click'>Get Started
+              </Button>
+              {/* <a href='/sign-in' onClick="hideMain()" className='button1'>
+                      Sign In
+            </a> */}
+            </Col>
+            <Col className='rightCol' s={6} md={4} px={0}>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    )
+  }
 }
 
-export default Landing
+export default withRouter(Landing)
