3aa98ca3db6db7fd46a581b5f79a77fdf0abf8d1
jackson916 <colingrey412@gmail.com>
2022-05-05 16:10:37 -0400

    chat comp

--- src/components/Chat/CreateChat.js
+++ src/components/Chat/CreateChat.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import io from 'socket.io-client'
+import './Chat.css'
 // import chat from '../../../../chat-app-react-back/app/models/chat'
 import { Button, InputGroup, FormControl } from 'react-bootstrap'
 import { createChat } from '../../api/chat'
 
 const socket = io('http://localhost:7165')
 
-class CreateChat extends React.Component {
+class CreateChat extends Component {
   constructor (props) {
     super(props)
+    console.log(this.props)
 
     this.state = {
       body: []
@@ -25,34 +27,38 @@ class CreateChat extends React.Component {
     socket.close()
   }
 
-  handleSubmit () {
+  handleSubmit (props) {
     // event.preventDefault()
-    socket.emit('chat', {
-      body: ''
-    })
-    createChat()
-      .then((msgAlert) => {
-        msgAlert({
-          heading: 'Message',
-          message: 'Message Sent!',
-          variant: 'success'
-        })
-      })
-      .catch((error, msgAlert) => {
-        msgAlert({
-          heading: 'Message failed to send',
-          message: 'Message error: ' + error.message,
-          variant: 'danger'
-        })
-      })
+    // socket.emit('chat', {
+    //   body: ''
+
+    const { user } = props
+    const data = {
+      body: ' '
+    }
+    createChat(data, user)
+    // .then((msgAlert) => {
+    //   msgAlert({
+    //     heading: 'Message',
+    //     message: 'Message Sent!',
+    //     variant: 'success'
+    //   })
+    // })
+    // .catch((error, msgAlert) => {
+    //   msgAlert({
+    //     heading: 'Message failed to send',
+    //     message: 'Message error: ' + error.message,
+    //     variant: 'danger'
+    //   })
+    // })
   }
 
   render () {
     return (
       <div>
-        <InputGroup size='sm' className="justifyContent: 'space-between'">
+        <InputGroup size='large' className="chat">
           <InputGroup.Text>
-            <Button variant='outline-secondary' onClick={this.handleSubmit}>
+            <Button variant='outline-secondary' onClick={this.handleSubmit(this.props)}>
                   Send
             </Button>
           </InputGroup.Text>
