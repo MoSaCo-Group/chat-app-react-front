748d970815a65fa29df330c5a0b345a574da44a7
SakoPak <sako.taya@gmail.com>
2022-05-11 22:14:45 -0700

    Built out createChat function
    imported SocketWorker

--- src/components/Chat/InputChat.js
+++ src/components/Chat/InputChat.js
@@ -1,13 +1,14 @@
 // state with 1 Message
+
 import React, { Component } from 'react'
 import { Button, Form, Card } from 'react-bootstrap'
 import { createChat } from '../../api/chat'
-// import axios from 'axios'
-// import apiUrl from '../../apiConfig'
-import io from 'socket.io-client'
 import './InputChat.css'
-
-const socket = io('http://localhost:7165/')
+import {
+  initiateSocketConnection,
+  disconnectSocket,
+  listenForChatMessage
+} from './SocketWorker'
 
 class InputChat extends Component {
   constructor (props) {
@@ -16,15 +17,21 @@ class InputChat extends Component {
     this.state = {
       body: ' '
     }
+    // this.socket = io('http://localhost:4741')
   }
 
-  // do we need socket connection on each component?
   // listening for chat messages emitted back from the server
   componentDidMount () {
-    socket.on('chat message', (data) => this.setState({ response: data }))
+    console.log('component did mount')
+    // open socket connection
+    initiateSocketConnection()
+    // listen for message from server
+    listenForChatMessage()
+    console.log('listening from server')
+    // listening to the 'emit axios' call from router that is sending back the created chat message.
+    // this.socket.on('chat message', (data) => this.setState({ response: data }))
+    return () => disconnectSocket()
   }
-
-  // message panel??
 
 handleChange = (event) => {
   console.log('entering text')
@@ -38,35 +45,58 @@ handleChange = (event) => {
 handleSubmit = (event) => {
   event.preventDefault()
   console.log('submit button clicked')
-  if (this.state.body) {
-    socket.emit('chat message', this.state.body)
-    // this.setState({ body: ' ' })
-  }
 
-  const { user } = this.props
+  const { user, msgAlert } = this.props
+
   createChat(this.state.body, user)
-  console.log('axios call performed')
+    .then((res) => {
+      console.log(res)
+      msgAlert({
+        heading: 'Chat created',
+        message: 'Chat sent!',
+        variant: 'success'
+      })
+      return res
+    })
+    .then((res) => {
+      console.log(res.data.chat.body)
+      // put messages into an array, so we can map through them later for display
+      const messages = this.setState([{ res: res.data.chat.body }])
+      return messages
+    })
+
+    .catch((error) => {
+      msgAlert({
+        heading: 'Chat creation failed',
+        message: 'Chat creation error: ' + error.message,
+        variant: 'danger'
+      })
+      console.log('axios call performed')
+    })
+
   // listening to all events emitted from server
-  socket.on('chat message', (message) => {
-    console.log('From server: ', message)
-  })
+  // this.socket.on('chat message', (message) => {
+  //   console.log('From server: ', message)
+  // })
 }
 
-// socket.on('chat message', function(msg)) {
-
-// }
-
 render () {
-  const { response } = this.state
   return (
+  // <><Toast className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  //   <Toast.Header className="toast-header">
+  //     <strong className="me-auto">Bootstrap</strong>
+  //     <small className="text-muted">just now</small>
+  //     <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  //   </Toast.Header>
+  //   <Toast.Body className="toast-body">
+  //     {response}
+  //   </Toast.Body>
+  // </Toast>
     <>
       <Card className='messageDisplay'>
-        <Card.Header></Card.Header>
+        <Card.Header>Message from User</Card.Header>
         <Card.Body>
-          <Card.Title>Display Chat</Card.Title>
-          <div>
-            <p>{response}</p>
-          </div>
+          <div>Messages will go here</div>
         </Card.Body>
       </Card>
       <Card className='body'>
@@ -89,4 +119,5 @@ render () {
   )
 }
 }
+
 export default InputChat
