ac31c36b75f6f44e7a827c826df616cdfea4cb26
SakoPak <sako.taya@gmail.com>
2022-05-19 16:11:31 -0700

    Added Socket
    Co-authored-by: Moe Suleiman moesuleiman8@gmail.com
    Co-authored-by: Colin Jackson colingrey412@gmail.com

--- src/components/Chat/InputChat.js
+++ src/components/Chat/InputChat.js
@@ -1,12 +1,12 @@
 
 import React, { Component } from 'react'
 import { Button, Form } from 'react-bootstrap'
-import { createChat, indexChat } from '../../api/chat'
 import './InputChat.css'
 import {
   initiateSocketConnection,
   disconnectSocket,
-  listenForChatMessage
+  createClientMessage,
+  onReceiveMessage
 } from './SocketWorker'
 import ScrollToBottom from 'react-scroll-to-bottom'
 
@@ -25,12 +25,24 @@ class InputChat extends Component {
     console.log('component did mount')
     // open socket connection
     initiateSocketConnection()
-    // listen for message from server
-    listenForChatMessage()
-    console.log('listening from server')
-    // listening to the 'emit axios' call from router that is sending back the created chat message.
+    // listen for message from server and receiving message data
+    // onReceiveMessage((data) => this.setState({ messages: data }))
+    onReceiveMessage(data => this.setState(prevState => ({ messages: [...prevState.messages, { data }] } // don't touch this!!!
+    )))
+
+    // onReceiveMessage((data) =>
+    // this.setState((prevState) => ({
+    // messages: [...prevState.messages, { data: data.messages }],
+    // })) // don't touch this!!!
+    // )
+    // socket.on('server message', (data) => this.setState(prevState => ({ messages: [...prevState.messages, { data: data.messages }] })))
+    // onReceiveMessage(data => this.setState(prev => ({ messages: [...prev, data] })))
+
     // this.socket.on('chat message', (data) => this.setState({ response: data }))
-    return () => disconnectSocket()
+  }
+
+  componentWillUnmount () {
+    disconnectSocket()
   }
 
 handleChange = (event) => {
@@ -47,51 +59,79 @@ handleSubmit = (event) => {
 
   console.log('submit button clicked')
 
-  const { user, msgAlert } = this.props
+  const { user } = this.props
 
-  createChat(this.state.body, user)
-    .then((res) => {
-      console.log(res)
-      console.log(res.data.chat._id)
-      msgAlert({
-        heading: 'Chat created',
-        message: 'Chat sent!',
-        variant: 'success'
-      })
-      return res
-    })
-    .then(() => {
-      this.setState({ body: '' })
-    })
-    .catch((error) => {
-      msgAlert({
-        heading: 'Chat creation failed',
-        message: 'Chat creation error: ' + error.message,
-        variant: 'danger'
-      })
-    })
+  createClientMessage(this.state.body, user)
 
-  indexChat(user)
-    .then((res) => this.setState({ messages: res.data.chat }))
-    .then(() => console.log(this.state.messages))
+  this.setState({ body: '' })
+
+  // .then((res) => {
+  //   console.log(res)
+  //   console.log(res.data.chat._id)
+  // //   msgAlert({
+  // //     heading: 'Chat created',
+  // //     message: 'Chat sent!',
+  // //     variant: 'success'
+  // //   })
+  // //   return res
+  // })
+  // .then(() => {
+  //   this.setState({ body: '' })
+  // })
+  // .catch((error) => {
+  //   msgAlert({
+  //     heading: 'Chat creation failed',
+  //     message: 'Chat creation error: ' + error.message,
+  //     variant: 'danger'
+  //   })
+  // })
+
+  // createChat(this.state.body, user)
+  //   .then((res) => {
+  //     console.log(res)
+  //     console.log(res.data.chat._id)
+  //     msgAlert({
+  //       heading: 'Chat created',
+  //       message: 'Chat sent!',
+  //       variant: 'success'
+  //     })
+  //     return res
+  //   })
+  //   .then(() => {
+  //     this.setState({ body: '' })
+  //   })
+  //   .catch((error) => {
+  //     msgAlert({
+  //       heading: 'Chat creation failed',
+  //       message: 'Chat creation error: ' + error.message,
+  //       variant: 'danger'
+  //     })
+  //   })
+
+  // indexChat(user)
+  //   .then((res) => this.setState({ messages: res.data.chat }))
+  //   .then(() => console.log(this.state.messages))
 }
 
 render () {
+  // const messageJsx = this.state.messages.map((message) => (
+  //   <Message key={message.id} message={message} />
+  // ))
+
   const { messages } = this.state
   const { user } = this.props
   let messagesJSX
   if (!messages) {
     messagesJSX = 'Loading...'
   } else {
-    messagesJSX = messages.map((message) => {
-      if (message.owner === user._id) {
-        return <div className='owner-message' key={message._id}>{message.body}</div>
+    messagesJSX = messages.map((message, index) => {
+      if (message.data.user._id === user._id) {
+        return <div className='owner-message' key={index}>{message.data.body}</div>
       } else {
-        return <div className='each-message' key={message._id}>{message.body}</div>
+        return <div className='each-message' key={index}>{message.data.body}</div>
       }
     })
   }
-
   return (
     <>
       <div className='main-container'>
